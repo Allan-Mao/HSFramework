c35ff59f10cc0bb47f623deeacafdc2451ed42fe
Ilnar Aliullov <ilnar.aliullov@gmail.com>
2014-11-19 07:08:23 +0300

    Rename facade header.

---
+++ HSFFacade.h
@@ -0,0 +1,13 @@
+//
+//  HSFFacade.h
+//  HSFramework
+//
+//  Created by Ilnar Aliullov on 07/05/14.
+//  Copyright (c) 2014 Ilnar Aliullov. All rights reserved.
+//
+
+/*!
+ @abstract Common header for whole HSFramework.
+ */
+
+#import "HSFClient.h"
